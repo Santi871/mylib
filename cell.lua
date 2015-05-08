21f0bef3baa91df3779ec2c97f0f8b974865940b
Santi871 <santivegega@gmail.com>
2015-05-08 17:53:29 -0300

    updated cell.lua

--- cell.lua
+++ cell.lua
@@ -1,45 +1,37 @@
 local component = require("component")
-local keyboard = require("keyboard")
 local term = require("term")
-local gpu1 = component.gpu
-local sides = require("sides")
-local colors = require("colors")
-local rs = component.redstone
 local comp = require("comp")
- 
-local function round(num, idp)
-  local mult = 10^(idp or 0)
-  return math.floor(num * mult + 0.5) / mult
-end
- 
-t = comp.getTurbines()
- 
-term.clear()
- 
-r1 = io.read()
-r2 = io.read()
-r3 = io.read()
- 
-comp.enableAllTurbines(t)
- 
+local keyboard = require("keyboard")
+local modem = component.modem
+local gpu = component.gpu
+
+local CALIBRATION_OFFSET = 21
+
+local w, h = gpu.getResolution()
+
+gpu.setResolution(w/6, h/6)
+
+cells = comp.getComponent("tile_thermalexpansion_cell_resonant_name")
+
+maxEnergy = component.proxy(cells[1]).getMaxEnergyStored()
+
+lastEnergy = component.proxy(cells[1]).getEnergyStored()
+
 while true do
- 
-tableFlow = pid(t[1],t[2],t[3],r1,r2,r3)
- 
- 
-comp.toTurbine(t,1).setFluidFlowRateMax(tableFlow[1])
-comp.toTurbine(t,2).setFluidFlowRateMax(tableFlow[2])
- 
-if keyboard.isKeyDown(keyboard.keys.space) then term.clear() os.exit() end
- 
-gpu1.fill(1,1,999,999," ")
- 
-gpu1.set(1,1, "Current turbine 1 output: " .. math.floor(comp.toTurbine(t,1).getEnergyProducedLastTick()) .. " RF/t")
-gpu1.set(1,2, "Current rotor 1 speed: " .. round(comp.toTurbine(t,1).getRotorSpeed(),1) .. " RPM")
-gpu1.set(1,3, "Target rotor 1 speed: " .. round(r1,1) .. " RPM")
- 
-gpu1.set(1,9, "Current turbine 1 flow rate: " .. math.floor(tableFlow[1]))
- 
-os.sleep(0.5)
- 
+
+ currentEnergy = component.proxy(cells[1]).getEnergyStored()
+ outputEnergy = ((currentEnergy - lastEnergy)/20)
+ lastEnergy = currentEnergy
+
+ finalEnergy = comp.round(outputEnergy - (outputEnergy/CALIBRATION_OFFSET),0)
+ chargePercentage = comp.round(currentEnergy/maxEnergy*100,0)
+
+ gpu.fill(1,1,999,999," ")
+ gpu.set(1,1,"Energy gain: "..finalEnergy.." RF/t")
+ gpu.set(1,2,'Charge percentage: '..chargePercentage..'%')
+
+ if keyboard.isKeyDown(keyboard.keys.space) then gpu.setResolution(w, h) term.clear() os.exit() end
+
+os.sleep(1)
+
 end
