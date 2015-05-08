90854ad6064ca3faceeedc0b1571bc82578b4e7c
Santi871 <santivegega@gmail.com>
2015-05-08 15:48:06 -0300

    added comp.getComponent(s)

--- comp2.lua
+++ comp2.lua
@@ -16,45 +16,54 @@ end
 b = {}
 slot = 1
 
+function comp.getComponent(s)
+  local f = component.list(tostring(s))
+  local b = {}
+  local slotB = 1
+  for k in f do table.insert(b, slotB, tostring(k)) slotB = slotB + 1 end
+  return f
+end
+
+
 function comp.getTurbines()
 
         t = component.list("br_turbine")
         local b = { }
         local slot = 1
-        for k in t do table.insert(b, slot, tostring(k)) slot = slot + 1 end   
+        for k in t do table.insert(b, slot, tostring(k)) slot = slot + 1 end
         return b
-       
+
 end
 
 function comp.toTurbine(t,n)
- 
+
   turbine = component.proxy(t[n])
   return turbine
- 
+
 end
 
 function  comp.disableAllTurbines(t)
- 
+
   local c = tableLength(t)
- 
+
    for i=1,c do
- 
+
     component.proxy(t[i]).setActive(false)
- 
+
    end
- 
+
 end
- 
+
 function comp.enableAllTurbines(t)
- 
+
   local c = tableLength(t)
- 
+
     for i=1,c do
- 
+
       component.proxy(t[i]).setActive(true)
- 
+
     end
- 
+
 end
 
 function setTarRPM(_, _, _, _, _, v)
@@ -75,7 +84,7 @@ out = {}
 dErr = 0
 curFlowDev = {}
 lastError = 0
-	
+
 for i=1, c do
 	curErrors[i] = 0
 	out[i] = 0
@@ -87,7 +96,7 @@ for i=1, c do
 end
 
 function comp.turbinePID()
-		
+
 	for i=1, c do
 
 			curOut = component.proxy(turbines[i]).getEnergyProducedLastTick()
@@ -96,22 +105,22 @@ function comp.turbinePID()
 			proErrors[i] = 10*curErrors[i]
 			errSums[i] = errSums[i] + curErrors[i]
 			dErrs[i] = curErrors[i] - errors[i]
-			
-			
+
+
 			if errSums[i] > 2000 then errSums[i]=2000 end
 			if errSums[i] < -2000 then errSums[i]=-2000 end
-				
- 
+
+
 			curFlowDev[i] = proErrors[i] + errSums[i] + dErrs[i]
-			
+
 			out[i] = curFlowDev[i]
- 
+
 			errors[i] = curErrors[i]
-	
+
 		end
 
 	return out
-	
+
 end
 
 return comp
